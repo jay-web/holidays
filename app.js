909fef8030aa9f19487d5f8c2435c57de9bbabe7
apple <apple@apples-MacBook-Pro-2.local>
2020-09-30 00:30:12 +0530

    Added GET request route for tours collection resource

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
-
+const fs = require("fs");
 const app = express();
 
-app.get("/", (req, res ) => {
+// fetch tours collection
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+
+app.get("/api/v1/tours", (req, res) => {
     res
         .status(200)
-        .json({app: "Holidays"});
+        .json(
+            {   status: "success",
+                data : {
+                    tours: tours}
+                }
+            )
 })
 
 
