763bae329b65288d01d5b90095419b064f2873b0
apple <apple@apples-MacBook-Pro-2.local>
2020-09-30 01:18:29 +0530

    Added POST request route for tours resource to create new tour in collection

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
-// fetch tours collection
+app.use(express.json());    // middleware to read req.body
+
+// fetch tours collection from local db file
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
-
+// * GET request of tours collection resource
 app.get("/api/v1/tours", (req, res) => {
     res
         .status(200)
@@ -17,7 +19,24 @@ app.get("/api/v1/tours", (req, res) => {
             )
 })
 
+app.post("/api/v1/tours", (req, res) => {
+    let newid = tours.length;
+    let newTour = Object.assign({id: newid}, req.body);
 
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err,data) => {
+        res
+            .status(201)
+            .json({
+                status: "success",
+                data: {
+                    tour: newTour
+                }
+            })
+    });
+
+})
 
 
 
