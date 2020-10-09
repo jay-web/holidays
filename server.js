a53059327bfef2328a147222538c425709209941
apple <apple@apples-MacBook-Pro-2.local>
2020-10-09 19:51:17 +0530

    Added mongoose driver packages to connect express app with mongodb server and create the collection and documents

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const mongoose = require("mongoose");
 const dotenv = require('dotenv');
-dotenv.config({ path: "./config.env"});
+dotenv.config({ path: "./config.env"});     // * to fetch config items from file
 
-const app = require("./app");
+// * It is just to replace database password dynamically
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
 
-console.log(process.env.NODE_ENV);
+// * It is to connect with cloud database 
+// * to change to local database replace DB with process.env.DATABASE_LOCAL
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then((cons) => console.log(cons.connection));
+
+const app = require("./app");       // app from app.js running express
+
 
 const port = process.env.PORT ;
 app.listen(port, () => {
