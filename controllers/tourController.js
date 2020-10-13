6ab4608846e47d58257afe742075c3428daefd4f
apple <apple@apples-MacBook-Pro-2.local>
2020-10-13 19:18:54 +0530

    Added mongoose.findByIdAndDelete function to delete any tour by id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -111,7 +111,7 @@ exports.updateTour = async (req, res) => {
       }
     })
   }catch(error){
-    res.status(400).json({
+    res.status(404).json({
       status: "failed",
       message: error.message
     })
@@ -120,7 +120,19 @@ exports.updateTour = async (req, res) => {
  
 };
 
-exports.deleteTour = (req, res) => {
-  let id = req.params.id * 1; // convert string into int
+exports.deleteTour = async (req, res) => {
+ 
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null
+    })
+  }catch(error){
+     res.status(404).json({
+       status: "failed",
+       message: error.message
+     })
+  }
   
 };
