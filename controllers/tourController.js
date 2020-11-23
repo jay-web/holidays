0449ef42e0e1f1660dd91452ce98cb7a6d3ebb68
apple <apple@apples-MacBook-Pro-2.local>
2020-11-23 17:46:36 +0530

    commented code deleted

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -9,48 +9,17 @@ const Tour = require("./../models/tourModel");
 const APIFeatures = require("./../utils/apiFeatures");
 
 // * params middleware to check valid passed id
-// exports.checkId = (req, res, next, val) => {
-//   console.log(typeof val);
-//   let tour = tours.find((el) => {
-//     return el.id === val * 1;
-//   });
-
-//   // if search tour not available as per id
-//   if (!tour) {
-//     return res.status(404).json({
-//       status: "failed",
-//       message: "Invalid id",
-//     });
-//   }
-
-//   next();
-// }
-
-// // * custom middleware to check req.body
-// exports.checkBody = (req, res, next) => {
-//   console.log(req.body);
-//   if(!req.body.name){
-//     return res.status(400).json({
-//       status: "failed",
-//       message: "name is mandatory in new tour"
-//     })
-//   }
-//   next();
-// }
-
-
 
 
 // Tours Route handlers/controllers
 exports.getAllTours = async (req, res) => {
   try {
-   
     // * Execute the query
     const features = new APIFeatures(Tour.find(), req.query)
-                                    .filter()
-                                    .sort()
-                                    .limitFields()
-                                    .pagination();
+      .filter()
+      .sort()
+      .limitFields()
+      .pagination();
 
     const tours = await features.query;
     res.status(200).json({
@@ -142,8 +111,6 @@ exports.deleteTour = async (req, res) => {
   }
 };
 
-
-
 // * Top five cheap tour filter middleware
 exports.aliesTopFive = (req, res, next) => {
   req.query.limit = "5";
@@ -151,5 +118,4 @@ exports.aliesTopFive = (req, res, next) => {
   req.query.fields = "name,duration,ratingsAverage,difficulty,summary,price";
 
   next();
-
-}
+};
