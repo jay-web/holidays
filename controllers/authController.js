4d2df73c6bc224041af5a65760c1babc1a6bb4e7
jay-web <jay.developer12@gmail.com>
2020-11-27 23:25:07 +0530

    Added jsonwebtoken package to generate jwt, created token in signup auth controller and send the token with signup route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,26 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 exports.signUp = async (req, res) => {
     try{
-        const newUser = await User.create(req.body);
+        // const newUser = await User.create(req.body);
+
+        const newUser = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirm: req.body.passwordConfirm
+        });
+
+        const token = jwt.sign(
+            {id: newUser._id, name: "welcome_in_holidays"}, 
+            process.env.JWT_SECRET, 
+            { expiresIn: process.env.JWT_EXPIRES_IN }
+            );
 
         res.status(201).json({
             status: "success",
+            token,
             data: {
                 user: newUser
             }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,28 +1,39 @@
 // Users Route handlers/controllers
 const User = require("../models/userModel");
 exports.getAllUsers = async (req, res) => {
-  try{
+  try {
     const allUsers = await User.find();
 
     res.status(201).json({
-        status: "success",
-        totalUsers: allUsers.length,
-        data: {
-            users: allUsers
-        }
-    })
-}catch(error){
+      status: "success",
+      totalUsers: allUsers.length,
+      data: {
+        users: allUsers,
+      },
+    });
+  } catch (error) {
     res.status(400).json({
-        status: "failed",
-        message: error.message
-    })
-}
+      status: "failed",
+      message: error.message,
+    });
+  }
 };
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not created yet",
-  });
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: user
+      }
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: "This route is not created yet",
+    });
+  }
 };
 exports.createUser = (req, res) => {
   res.status(500).json({
