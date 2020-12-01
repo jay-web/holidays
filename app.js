72aa8dbb68c6d69d573b62b685ebc5ddc563af1d
jay-web <jay.developer12@gmail.com>
2020-12-01 17:55:10 +0530

    Added error controller, and middleware to handle operation errors

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 const morgan = require("morgan");
+
+const globalErrorHandler = require("./controllers/errorController");
+const AppError = require("./utils/appError");
 const tourRouter = require("./routes/tourRouter");
 const userRouter = require("./routes/userRouter");
 
@@ -16,5 +19,13 @@ app.use(express.static(`${__dirname}/public`));
 // * Applying middleware as per requested route
 app.use("/api/v1/users", userRouter);
 app.use('/api/v1/tours', tourRouter);       // mounting routes
+
+// * Middleware to handle undefined routes
+app.all("*", (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server`));
+});
+
+// * Middleware to handle error
+app.use(globalErrorHandler);
 
 module.exports = app;
